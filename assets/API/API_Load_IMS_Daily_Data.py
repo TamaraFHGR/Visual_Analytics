4c7c459f9e5b03dc2a125d462cc5783973555c25
TamaraFHGR <162818989+TamaraFHGR@users.noreply.github.com>
2024-10-06 13:20:27 +0200

    store_daily_ims_data

--- assets/API/API_Load_IMS_Daily_Data.py
+++ assets/API/API_Load_IMS_Daily_Data.py
@@ -1,28 +1,20 @@
 import requests
 import pandas as pd
 
-# URL der API
+# URL and API-request:
 url = 'https://measurement-api.slf.ch/public/api/imis/measurements'
-
-# API-Aufruf
 response = requests.get(url)
 
-# Überprüfen, ob die Anfrage erfolgreich war
+# Check if the request was successful:
 if response.status_code == 200:
-    # JSON-Antwort verarbeiten
     data = response.json()
+    df_ims_d = pd.DataFrame(data) # Create a DataFrame from the JSON data
+    df_ims_d.name = '03_SLF_daily_ims_measurements'
+    csv_file_path = f"assets/{df_ims_d.name}.csv"
 
-    # In ein Pandas DataFrame umwandeln
-    df = pd.DataFrame(data)
-
-    # Speichern unter dem Namen '00_SLF_IMS_stations'
-    df.name = 'test_SLF_ims_measurements'
-
-    # Ausgabe des DataFrames
-    print(df.head())  # zeigt die ersten Zeilen an
+    # Append new data to CSV-File
+    df_ims_d.to_csv(csv_file_path, sep=';', index=True, mode='a',
+              header=True)  # header=False, to avoid duplicate headers
+    print(df_ims_d.head())
 else:
-    print(f"Fehler bei der Anfrage: {response.status_code}")
-
-# Datensatz speichern
-df.to_csv(f"assets/{df.name}.csv", sep=';', index=True)
-
+    print(f"Error during request: {response.status_code}")
