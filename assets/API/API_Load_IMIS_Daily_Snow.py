cd889ceeaf38903a4516f55847b9507bb7708d80
TamaraFHGR <162818989+TamaraFHGR@users.noreply.github.com>
2024-10-07 17:52:33 +0200

    change relative path for automated API update

--- assets/API/API_Load_IMIS_Daily_Snow.py
+++ assets/API/API_Load_IMIS_Daily_Snow.py
@@ -9,8 +9,9 @@ response = requests.get(url)
 if response.status_code == 200:
     data = response.json()
     df_imis_d = pd.DataFrame(data) # Create a DataFrame from the JSON data
-    df_imis_d.name = '05_SLF_daily_imis_snow'
-    csv_file_path = f"C:/Users/tamar/OneDrive/Dokumente/GitHub/Visual_Analytics/assets/{df_imis_d.name}.csv"
+    df_imis_d.name = 'yy_SLF_daily_imis_snow'
+    #csv_file_path = f"C:/Users/tamar/OneDrive/Dokumente/GitHub/Visual_Analytics/assets/{df_imis_d.name}.csv"
+    csv_file_path = f"./daily/{df_imis_d.name}.csv"
 
     # Append new data to CSV-File
     df_imis_d.to_csv(csv_file_path, sep=';', index=True, mode='a',
