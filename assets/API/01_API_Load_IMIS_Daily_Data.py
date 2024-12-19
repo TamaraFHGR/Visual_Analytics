db85f59795d4f51e13e09249c8503792bed27601
TamaraFHGR <162818989+TamaraFHGR@users.noreply.github.com>
2024-12-19 10:06:26 +0100

    .

--- assets/API/01_API_Load_IMIS_Daily_Data.py
+++ assets/API/01_API_Load_IMIS_Daily_Data.py
@@ -1,20 +1,22 @@
 import requests
 import pandas as pd
 
-# URL and API-request:
+# URL mit Datum als Filterparameter
 url = 'https://measurement-api.slf.ch/public/api/imis/measurements'
-response = requests.get(url)
+params = {'measure_date': '2024-12-17'}  # Query-Parameter
 
-# Check if the request was successful:
+# API-Request mit Filter
+response = requests.get(url, params=params)
+
+# Überprüfung und Verarbeitung
 if response.status_code == 200:
     data = response.json()
-    df_imis_d = pd.DataFrame(data) # Create a DataFrame from the JSON data
-    df_imis_d.name = '04_SLF_daily_imis_measurements'
+    df_imis_d = pd.DataFrame(data)
+    df_imis_d.name = '04_SLF_daily_imis_measurements_17.12.24'
     csv_file_path = f"./daily/{df_imis_d.name}.csv"
 
-    # Append new data to CSV-File
-    df_imis_d.to_csv(csv_file_path, sep=';', index=True, mode='a',
-              header=False)  # header=False, to avoid duplicate headers
+    # Daten an CSV-Datei anhängen
+    df_imis_d.to_csv(csv_file_path, sep=';', index=True, mode='a', header=False)
     print(df_imis_d.head())
 else:
     print(f"Error during request: {response.status_code}")
