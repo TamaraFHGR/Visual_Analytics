8248ae7109f761962fa47944ac0220cdf5d35d2a
TamaraFHGR <162818989+TamaraFHGR@users.noreply.github.com>
2024-12-21 13:55:38 +0100

    Data Corrections

--- assets/API/03_Delete_Duplicates.py
+++ assets/API/03_Delete_Duplicates.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-measure_df = pd.read_csv('daily/04_SLF_daily_imis_measurements.csv', sep=';',skiprows=0, low_memory=False)
-snow_df = pd.read_csv('daily/05_SLF_daily_imis_snow.csv', sep=';',skiprows=0, low_memory=False)
+measure_df = pd.read_csv('daily/04_SLF_daily_imis_measurements.csv', sep=';',skiprows=0)
+snow_df = pd.read_csv('daily/05_SLF_daily_imis_snow.csv', sep=';',skiprows=0)
 
 # Delete duplicates, ignore the index and sort by station_code and measure_date:
 measure_df.drop_duplicates(subset=['station_code', 'measure_date'], inplace=True, ignore_index=True)
